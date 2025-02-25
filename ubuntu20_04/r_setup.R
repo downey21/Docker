73bc8c1826f7fcf857b7b7442c25e01cdc27b7b7
downey21 <downe7921@gmail.com>
2025-02-25 13:32:44 +0900

    Update ubuntu20_04

---
+++ ubuntu20_04/r_setup.R
@@ -0,0 +1,21 @@
+
+# coding: utf-8
+
+vector_package <-
+    c(
+        "languageserver",
+        "httpgd",
+        "dplyr",
+        "tidyr",
+        "stringr",
+        "tibble",
+        "lubridate",
+        "ggsignif",
+        "ggplot2"
+    )
+
+utils::install.packages(
+    pkgs = vector_package,
+    dependencies = TRUE,
+    repos = "https://cran.yu.ac.kr/"
+)
